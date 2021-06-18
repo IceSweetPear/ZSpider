578c5dc08b09c0dd3c34b20f3e1b7339c1ad5349
zzz <157323060@qq.com>
2021-06-18 15:26:39 +0800

    add

--- web_hook.php
+++ web_hook.php
@@ -1,20 +1,4 @@
 
 <?php
-/**
- * Description:钩子
- * Created by PhpStorm.
- * User: Vijay <1937832819@qq.com>
- * Date: 2020/4/29
- * Time: 22:27
- */
 
-echo "zzz";
-
-// 接收码云POST过来的信息
-$json = $GLOBALS['HTTP_RAW_POST_DATA'];
-
-// 请求ip
-$client_ip = $_SERVER['REMOTE_ADDR'];
-
-// 执行shell命令,cd到网站根目录，执行git pull进行拉取代码，并把返回信息写进日志
 exec('cd /www/wwwroot/zsplider; git pull;echo "zzz"');
